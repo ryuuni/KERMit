10c372e212a04178c07297666c04aa6500075d7c
riddhima25 <riddhima.narravula@gmail.com>
2020-11-15 16:25:03 -0800

    clean up and set up router

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,58 @@
 import './App.css';
 import GoogleBtn from './GoogleBtn';
+import React, { Component } from 'react'
+import Leaderboard from './leaderboard/Leaderboard'
+import Puzzles from './puzzle/Puzzles'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App" data-testid="app">
-      <div data-testid="login-btn">
-        <GoogleBtn />
-      </div>
-    </div>
-  );
+class App extends Component {  
+  constructor() {
+    super()
+    this.state = {
+      accessToken: '',
+    }
+  }
+
+  render() {
+    const loggedIn = this.state.accessToken !== "";
+    return (
+      <Router>
+        <div className="App" data-testid="app">
+          <div className="header">
+            <div className="title">Isshoni Sudoku</div>
+            {loggedIn && (
+              <div>
+                <Link to="/mypuzzles">My Puzzles</Link>
+                <Link to="/leaderboard">Leaderboard</Link>
+              </div>
+            )}
+            <div className="login-btn" data-testid="login-btn">
+              <GoogleBtn onAccessTokenChanged={accessToken => this.setState({accessToken})}/>
+            </div>
+          </div>
+          <div className="page-content">
+            {loggedIn ? (
+              <Switch>
+                <Route path="/mypuzzles">
+                  <Puzzles accessToken={this.state.accessToken}/>
+                </Route>
+                <Route path="/leaderboard">
+                  <Leaderboard accessToken={this.state.accessToken}/>
+                </Route>
+              </Switch>
+            ):(
+              <div>Please log in!</div>
+            )}
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ app/src/leaderboard/Leaderboard.js
@@ -0,0 +1,5 @@
+function Leaderboard(props) {
+    return <h2>Leaderboard</h2>;
+}
+
+export default Leaderboard;
