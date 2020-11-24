6cbee666afb9f3f77dce0d14bbc73c9759a8a696
Emily Jin <ej2332@columbia.edu>
2020-11-24 11:07:27 -0500

    add export to Endpoint

--- app/src/utils/Endpoint.js
+++ app/src/utils/Endpoint.js
@@ -3,10 +3,12 @@ const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT ?? '5000';
 
 const backendRoot = `http://${BACKEND_ADDRESS}:${BACKEND_PORT}`;
 
-export const Endpoint = {
+const Endpoint = {
   register: () => `${backendRoot}/register`,
   createPuzzle: ({difficulty, size = 3}) => `${backendRoot}/puzzles?difficulty=${difficulty}&size=${size}`,
   getPuzzles: () => `${backendRoot}/puzzles`,
   movePiece: ({puzzleId}) => `${backendRoot}/puzzles/${puzzleId}/piece`,
   getLoaderboard: () => `${backendRoot}/leaderboard`,
 };
+
+export default Endpoint;
