f322c411ceb4ec798ea6692e8bd8815d066e02d1
riddhima25 <riddhima.narravula@gmail.com>
2020-11-25 12:02:39 -0800

    cleaning up Socket.js

--- app/src/utils/Socket.js
+++ app/src/utils/Socket.js
@@ -1,8 +1,4 @@
 import io from "socket.io-client";
-// import { SOCKET_URL } from "config";
-// import openSocket from "socket.io-client";
 
 const ENDPOINT = "ws://127.0.0.1:5000/";
-// const socket = openSocket('http://localhost:5000', {transports: ['websocket']});
 export const socket = io(ENDPOINT, {transports: ['websocket']});
-// export const socket = new WebSocket(ENDPOINT);
