ef1fb2ad42aa5f414500837e1ef053557f31d394
Emily Jin <ej2332@columbia.edu>
2020-12-03 06:17:57 -0500

    refactor socket code in PuzzlePage

--- app/src/pages/Puzzle/PuzzlePage.js
+++ app/src/pages/Puzzle/PuzzlePage.js
@@ -35,6 +35,8 @@ const PuzzlePage = () => {
         (pieceA.y_coordinate * 10 + pieceA.x_coordinate) - (pieceB.y_coordinate * 10 + pieceB.x_coordinate)
     ));
     setSolved(completed);
+    console.log('PLAYERS:');
+    console.log(players);
     if (players !== undefined) {
       setPlayers(players);
     }
@@ -43,58 +45,89 @@ const PuzzlePage = () => {
   const addMessage = useCallback((data) => {
     setMessages(oldMessages => [...oldMessages, data]);
   }, []);
-
-  useEffect(() => {  
-    socket.current = socketIOClient("ws://127.0.0.1:5000/", {query: {auth: accessToken}, transports: ['websocket']});
-
-    const currSocket = socket.current;
-
-    if (currSocket.disconnected) {
-      currSocket.connect({query: {auth: accessToken}});
-    }
-    currSocket.emit('join', {puzzle_id: puzzleId, token: accessToken});
-
-    currSocket.on("puzzle_update", ({pieces, completed}) => {
-      console.log(pieces);
-      console.log(completed);
-      updatePuzzle({pieces, completed})
-    });
-
-    currSocket.on("message_update", (data) => {
-      console.log('got message:');
-      console.log(data);
-      addMessage(data);
-    });
-
-    currSocket.on('lock_update_add', ({x_coordinate: x, y_coordinate: y, player}) => {
-      setPlayersLockingCells({
-        ...playersLockingCells, 
-        [coordsToString(x, y)]: {player, index: players.findIndex(p => p.id === player.id)}
-      });
-    });
   
-    currSocket.on('lock_update_remove', ({x_coordinate: x, y_coordinate: y}) => {
-      const newPlayersLockingCells = {...playersLockingCells};
-      delete newPlayersLockingCells[coordsToString(x, y)];
-      setPlayersLockingCells(newPlayersLockingCells);
-    });
-
-    currSocket.on("player_joined", data => {
-      console.log('player joined:');
-      console.log(data);
-    });
-
+  useEffect(() => {  
     getPuzzle({
       accessToken,
       puzzleId,
       onSuccess: updatePuzzle,
     });
+  }, []);
 
+  useEffect(() => {
+    const currSocket = socketIOClient("ws://127.0.0.1:5000/", {query: {auth: accessToken}, transports: ['websocket']});
+    socket.current = currSocket;
+
+    if (currSocket.disconnected) {
+      currSocket.connect({query: {auth: accessToken}});
+    }
+    currSocket.emit('join', {puzzle_id: puzzleId, token: accessToken});
+    
     return () => {
       currSocket.emit('leave', {puzzle_id: puzzleId});
       currSocket.disconnect();
     };
-  }, [accessToken, puzzleId, socket, updatePuzzle, addMessage, playersLockingCells, setPlayersLockingCells, players]);
+  }, []);
+
+  useEffect(() => {  
+    socket.current.on("puzzle_update", ({pieces, completed}) => {
+      console.log(pieces);
+      console.log(completed);
+      updatePuzzle({pieces, completed})
+    });
+
+    return () => {
+      socket.current.removeAllListeners('puzzle_update');
+    };
+  }, [socket, updatePuzzle]);
+
+  useEffect(() => {  
+    socket.current.on("message_update", (data) => {
+      console.log('got message:');
+      console.log(data);
+      addMessage(data);
+    });
+
+    return () => {
+      socket.current.removeAllListeners('message_update');
+    };
+  }, [socket, addMessage]);
+
+  useEffect(() => {  
+    socket.current.on('lock_update_remove', ({x_coordinate: x, y_coordinate: y}) => {
+      const newPlayersLockingCells = {...playersLockingCells};
+      delete newPlayersLockingCells[coordsToString(x, y)];
+      setPlayersLockingCells(newPlayersLockingCells);
+    });
+
+    return () => {
+      socket.current.removeAllListeners('lock_update_remove');
+    };
+  }, [socket, playersLockingCells]);
+
+  useEffect(() => {  
+    socket.current.on('lock_update_add', ({x_coordinate: x, y_coordinate: y, player}) => {
+      setPlayersLockingCells({
+        ...playersLockingCells, 
+        [coordsToString(x, y)]: {player, index: players.findIndex(p => p.id === player.id)}
+      });
+    });
+
+    return () => {
+      socket.current.removeAllListeners('lock_update_add');
+    };
+  }, [socket, players, playersLockingCells]);
+
+  useEffect(() => {  
+    socket.current.on("player_joined", data => {
+      console.log('player joined:');
+      console.log(data);
+    });
+
+    return () => {
+      socket.current.removeAllListeners('player_joined');
+    };
+  }, [socket]);
 
   return (
     <PageTemplate>
